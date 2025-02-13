9a89c96a74c5aacadf98e03820380d80ed05912e
Alex Jenkins <alex@relevanceai.com>
2025-02-14 09:40:53 +1100

    examples

--- examples/from_local_to_relevanceai.py
+++ examples/from_local_to_relevanceai.py
@@ -1,9 +1,9 @@
 from manage_aiworkforce.agent import create_agent
+from manage_aiworkforce.knowledge import create_knowledge
 from manage_aiworkforce.tool import create_tools
 from manage_aiworkforce.utils import open_all_object_files
 
-def push_to_relevance_ai_prod():
-
+def push_to_relevance_ai_prod(region_id, prd_project_id, prd_api_key):
     agents = open_all_object_files("relevance_ai/agents")
     # knowledge = open_all_object_files("relevance_ai/knowledge")
     tools = open_all_object_files("relevance_ai/tools")
@@ -13,6 +13,13 @@ def push_to_relevance_ai_prod():
 
     create_tools(tools, region_id, prd_project_id, prd_api_key)
 
-
 if __name__ == "__main__":
-    push_to_relevance_ai_prod()
+    import os
+    from dotenv import load_dotenv
+    load_dotenv()
+
+    region_id = os.getenv("region_id")
+    prd_project_id = os.getenv("prd_project_id")
+    prd_api_key = os.getenv("prd_api_key")
+
+    push_to_relevance_ai_prod(region_id, prd_project_id, prd_api_key)
--- examples/from_relevanceai_to_local.py
+++ examples/from_relevanceai_to_local.py
@@ -3,15 +3,37 @@ from manage_aiworkforce.knowledge import get_all_knowledge
 from manage_aiworkforce.tool import get_all_tools
 from manage_aiworkforce.utils import save_all_objects, update_objects_metadata, remove_objects_changing_fields, remove_local_files_not_in_objects
 
-def get_current_state_from_relevance_ai():
-    
+def get_current_state_from_relevance_ai(region_id, dev_project_id, dev_api_key, prd_project_id):
+    # Sync Dev Relevance AI to local
     agents = get_all_agents(region_id, dev_project_id, dev_api_key)
+    tools = get_all_tools(region_id, dev_project_id, dev_api_key)
+    # knowledge = get_all_knowledge(region_id, dev_project_id, dev_api_key)
+
     agents = remove_objects_changing_fields(agents)
+    tools = remove_objects_changing_fields(tools)
+    # knowledge = remove_objects_changing_fields(knowledge)
+
     agents = update_objects_metadata(agents, prd_project_id)
+    tools = update_objects_metadata(tools, prd_project_id)
+    # knowledge = update_objects_metadata(knowledge, prd_project_id)
 
     remove_local_files_not_in_objects(agents, "agents", "relevance_ai/agents")
     save_all_objects(agents, "relevance_ai/agents", "agents")
 
+    remove_local_files_not_in_objects(tools, "tools", "relevance_ai/tools")
+    save_all_objects(tools, "relevance_ai/tools", "tools")
+
+    # remove_local_files_not_in_objects(knowledge, "knowledge", "relevance_ai/knowledge")
+    # save_all_objects(knowledge, "relevance_ai/knowledge", "knowledge")
 
 if __name__ == "__main__":
-    get_current_state_from_relevance_ai()
+    import os
+    from dotenv import load_dotenv
+    load_dotenv()
+
+    region_id = os.getenv("region_id")
+    dev_project_id = os.getenv("dev_project_id")
+    dev_api_key = os.getenv("dev_api_key")
+    prd_project_id = os.getenv("prd_project_id")
+
+    get_current_state_from_relevance_ai(region_id, dev_project_id, dev_api_key, prd_project_id)
