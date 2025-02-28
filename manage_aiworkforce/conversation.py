1b228ee0b6e97c68f0c518fa5bc6cd3a45ae7c83
Alex Jenkins <alex@relevanceai.com>
2025-02-28 16:50:33 +1100

    get conversations and conversations with failed tool filter functions

---
+++ manage_aiworkforce/conversation.py
@@ -0,0 +1,48 @@
+import requests
+import json
+
+from manage_aiworkforce.types import EventType, FilterType, ComparisonType
+
+
+def get_conversations(region_id:str, project_id:str, agent_id:str, api_key:str):
+    headers = {"Authorization": f"{project_id}:{api_key}"}
+    base_url = f"https://api-{region_id}.stack.tryrelevance.com/latest"
+    path = f"{base_url}/agents/conversations/list"
+    api_params = {
+        "page_size": 50000,
+        "filters": [{
+            "condition": "==",
+            "case_insensitive": False,
+            "field": "agent_id",
+            "filter_type": FilterType.EXACT_MATCH,
+            "condition_value": agent_id
+        }]
+    }
+    
+    response = requests.get(path, headers=headers, params=json.dumps(api_params))
+    return response.json()
+
+
+def get_conversations_where_specific_tool_failed(region_id:str, project_id:str, agent_id:str, tool_id:str, api_key:str):
+    headers = {"Authorization": f"{project_id}:{api_key}"}
+    base_url = f"https://api-{region_id}.stack.tryrelevance.com/latest"
+    path = f"{base_url}/agents/conversations/list"
+    api_params = {
+        "page_size": 50000,
+        "filters": [{
+            "condition": "==",
+            "case_insensitive": False,
+            "field": "agent_id",
+            "filter_type": FilterType.EXACT_MATCH,
+            "condition_value": agent_id
+        }],
+        "event_logs_filters": [{
+            "event_value": tool_id,
+            "event_type": EventType.TOOL_RUNS_FAILED,
+            "min_count": 1,
+            "max_count": 10,
+            "comparison_type": ComparisonType.GTE
+        }]
+    }
+    response = requests.get(path, headers=headers, params=json.dumps(api_params))
+    return response.json()
