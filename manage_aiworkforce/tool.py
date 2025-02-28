a6e9f46fdbc418af976c4fe91527659cd9790fc8
Alex Jenkins <alex@relevanceai.com>
2025-02-28 16:46:57 +1100

    added get tool history and trigger tool for rerun tool flow

--- manage_aiworkforce/tool.py
+++ manage_aiworkforce/tool.py
@@ -1,8 +1,8 @@
 import requests
 from manage_aiworkforce.utils import save_all_objects
+from manage_aiworkforce.types import FilterType
 
-
-def get_all_tools(region_id:str, project_id:str, api_key:str, limit:int=5000):
+def get_all_tools(region_id:str, project_id:str, api_key:str, limit:int=50000):
     headers = {"Authorization": f"{project_id}:{api_key}"}
     base_url = f"https://api-{region_id}.stack.tryrelevance.com/latest"
     path = f"{base_url}/studios/list"
@@ -11,7 +11,7 @@ def get_all_tools(region_id:str, project_id:str, api_key:str, limit:int=5000):
         headers=headers, 
         params={
             "page_size" : limit,
-            "filters" : f'[{{"field":"project","condition":"==","condition_value":"{project_id}","filter_type":"exact_match"}}]'
+            "filters" : f'[{{"field":"project","condition":"==","condition_value":"{project_id}","filter_type":{FilterType.EXACT_MATCH}}}]'
         }
     )
     return response.json()['results']
@@ -27,7 +27,33 @@ def create_tools(tool_jsons:list, region_id:str, project_id:str, api_key:str, pa
         "insert_if_not_exists": insert_if_not_exists
     }
     response = requests.post(path, json=payload, headers=headers)
-    
+    return response.json()
+
+
+def get_tool_run_history(tool_id:str, region_id:str, project_id:str, api_key:str):
+    headers = {"Authorization": f"{project_id}:{api_key}"}
+    base_url = f"https://api-{region_id}.stack.tryrelevance.com/latest"
+    path = f"{base_url}/studios/run_history/list"
+    payload = {
+        "page_size": 999999999999,
+        "filters": f'[{{"filter_type":{FilterType.EXACT_MATCH},"field":"project","condition":"==","condition_value":"{project_id}"}},{{"filter_type":{FilterType.EXACT_MATCH},"field":"studio_id","condition":"==","condition_value":"{tool_id}"}}]',
+        "with_agent_details": True
+    }
+    response = requests.get(path, headers=headers, params=payload)
+    return response.json()
+
+
+def trigger_tool(tool_id:str, region_id:str, project_id:str, api_key:str, tool_inputs:dict):
+    headers = {"Authorization": f"{project_id}:{api_key}"}
+    base_url = f"https://api-{region_id}.stack.tryrelevance.com/latest"
+    path = f"{base_url}/studios/{tool_id}/trigger_async" #
+    payload = {
+        "executor": {"type": "run_chain"},
+        "max_job_duration": "minutes",
+        "params": tool_inputs,
+        "studio_id": tool_id,
+    }
+    response = requests.post(path, headers=headers, json=payload)
     return response.json()
 
 
@@ -37,6 +63,7 @@ def delete_tools(tool_ids:list, region_id:str, project_id:str, api_key:str):
     path = f"{base_url}/studios/bulk_delete"
     response = requests.post(path, json={"ids": tool_ids}, headers=headers)
     return response.json()
+
 
 def save_tools_to_file(tools, folderpath):
     save_all_objects(tools, folderpath, "tools")
