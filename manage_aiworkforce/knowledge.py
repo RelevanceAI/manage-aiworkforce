89d3055e0ae68c5e6475705bba86a19959343836
Alex Jenkins <alex@relevanceai.com>
2025-03-02 23:48:10 +1100

    fixed knowledge to json

--- manage_aiworkforce/knowledge.py
+++ manage_aiworkforce/knowledge.py
@@ -9,7 +9,7 @@ def get_all_knowledge(region_id, project_id, api_key):
         "filters": [],
         "sort": [{"update_date":"desc"}]
     }
-    response = requests.post(path, body=body, headers=headers)
+    response = requests.post(path, json=body, headers=headers)
 
     return response.json().get("results", [])
 
@@ -23,7 +23,7 @@ def get_knowledge(region_id:str, project_id:str, api_key:str, knowledge_set: str
         "page_size": max_results,
         "sort": [{"insert_date_": "desc"}]
     }
-    response = requests.post(path, body=body, headers=headers)
+    response = requests.post(path, json=body, headers=headers)
     if response.status_code == 200:
         return response.json().get("results", [])
     return {"error": response.text}
@@ -34,7 +34,7 @@ def delete_knowledge(region_id:str, project_id:str, api_key:str, knowledge_set: 
     base_url = f"https://api-{region_id}.stack.tryrelevance.com/latest"
     path = f"{base_url}/knowledge/sets/delete"
     body = {"knowledge_set": knowledge_set}
-    response = requests.post(path, body=body, headers=headers)
+    response = requests.post(path, json=body, headers=headers)
 
     return response.status_code == 200
 
@@ -44,7 +44,7 @@ def create_knowledge(region_id:str, project_id:str, api_key:str, knowledge_set: 
     base_url = f"https://api-{region_id}.stack.tryrelevance.com/latest"
     path = f"{base_url}/knowledge/add"
     body = {"knowledge_set": knowledge_set}
-    response = requests.post(path, body=body, headers=headers)
+    response = requests.post(path, json=body, headers=headers)
 
     return response.status_code == 200
 
