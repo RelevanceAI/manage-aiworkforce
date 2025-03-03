e61c904f53480d07c414c5946c9811974a128b35
Alex Jenkins <alex@relevanceai.com>
2025-03-03 17:22:18 +1100

    added get conversation events (which is the cronological flow of tasks/messages)

--- manage_aiworkforce/conversation.py
+++ manage_aiworkforce/conversation.py
@@ -23,6 +23,18 @@ def get_conversations(region_id:str, project_id:str, agent_id:str, api_key:str):
     return response.json()
 
 
+def get_conversation_actions(region_id:str, project_id:str, agent_id:str, conversation_id:str, api_key:str):
+    headers = {"Authorization": f"{project_id}:{api_key}"}
+    base_url = f"https://api-{region_id}.stack.tryrelevance.com/latest"
+    path = f"{base_url}/agents/{agent_id}/tasks/{conversation_id}/view?full_history=true"
+    body = {}
+    response = requests.post(path, data=json.dumps(body), headers=headers)
+    result = response.json()
+    if 'results' in result:
+        result['results'] = sorted(result['results'], key=lambda x: x.get('insert_date_', ''), reverse=False)
+    return result
+
+
 def get_conversations_where_specific_tool_failed(region_id:str, project_id:str, agent_id:str, tool_id:str, api_key:str):
     headers = {"Authorization": f"{project_id}:{api_key}"}
     base_url = f"https://api-{region_id}.stack.tryrelevance.com/latest"
