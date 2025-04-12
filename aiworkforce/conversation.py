a85b95111d2ae995f98182c65c95ee0013f68baa
Alex Jenkins <alex@relevanceai.com>
2025-04-12 16:16:19 +1000

    added filtering by timestamp when getting conversations

--- aiworkforce/conversation.py
+++ aiworkforce/conversation.py
@@ -1,5 +1,8 @@
 import requests
 import json
+from datetime import datetime, timedelta
+
+import pytz
 
 from aiworkforce.types import EventType, FilterType, ComparisonType
 
@@ -63,31 +66,6 @@ def get_conversation_actions(region_id:str, project_id:str, agent_id:str, conver
     return result
 
 
-def get_conversations_where_specific_tool_failed(region_id:str, project_id:str, agent_id:str, tool_id:str, api_key:str):
-    headers = {"Authorization": f"{project_id}:{api_key}"}
-    base_url = f"https://api-{region_id}.stack.tryrelevance.com/latest"
-    path = f"{base_url}/agents/conversations/list"
-    api_params = {
-        "page_size": 50000,
-        "filters": json.dumps([{
-            "condition": "==",
-            "case_insensitive": False,
-            "field": "agent_id",
-            "filter_type": FilterType.EXACT_MATCH,
-            "condition_value": agent_id
-        }]),
-        "event_logs_filters": json.dumps([{
-            "event_value": tool_id,
-            "event_type": EventType.TOOL_RUNS_FAILED,
-            "min_count": 1,
-            "max_count": 10,
-            "comparison_type": ComparisonType.GTE
-        }])
-    }
-    response = requests.get(path, headers=headers, params=api_params)
-    return response.json()
-
-
 def retrigger_conversation_after_message(project_id:str, region_id:str, agent_id:str, conversation_id:str, message_id:str, api_key:str):
     headers = {"Authorization": f"{project_id}:{api_key}"}
     base_url = f"https://api-{region_id}.stack.tryrelevance.com/latest"
@@ -143,3 +121,77 @@ def get_trigger_message(region_id:str, project_id:str, agent_id:str, conversatio
           "triggered_by": trigger_message_data.get('content', {}).get('display', {}).get('name', None)
         }
     return {}
+
+
+def get_conversations_where_specific_tool_failed(region_id:str, project_id:str, agent_id:str, tool_id:str, api_key:str):
+    """ Variation of get_conversations with advanced filters """
+    headers = {"Authorization": f"{project_id}:{api_key}"}
+    base_url = f"https://api-{region_id}.stack.tryrelevance.com/latest"
+    path = f"{base_url}/agents/conversations/list"
+    api_params = {
+        "page_size": 50000,
+        "filters": json.dumps([{
+            "condition": "==",
+            "case_insensitive": False,
+            "field": "agent_id",
+            "filter_type": FilterType.EXACT_MATCH,
+            "condition_value": agent_id
+        }]),
+        "event_logs_filters": json.dumps([{
+            "event_value": tool_id,
+            "event_type": EventType.TOOL_RUNS_FAILED,
+            "min_count": 1,
+            "max_count": 10,
+            "comparison_type": ComparisonType.GTE
+        }])
+    }
+    response = requests.get(path, headers=headers, params=api_params)
+    return response.json()
+
+
+def get_conversations_between_dates(region_id, project_id, agent_id, api_key, from_dt=None, to_dt=None):
+    """ Variation of get_conversations with advanced filters """
+    headers = {"Authorization": f"{project_id}:{api_key}", "Content-Type": "application/json"}
+    base_url = f"https://api-{region_id}.stack.tryrelevance.com/latest"
+    path = f"{base_url}/agents/conversations/list"
+
+    filters = [
+        {
+            "field": "conversation.is_debug_mode_task",
+            "filter_type": "exact_match",
+            "condition": "!=",
+            "condition_value": True
+        },
+        {
+            "filter_type": "exact_match",
+            "field": "conversation.agent_id",
+            "condition_value": [agent_id],
+            "condition": "=="
+        }
+    ]
+    # has to be in these exact formats
+    if from_dt:
+        filters.append({
+            "filter_type": "numeric",
+            "field": "update_datetime",
+            "condition": ">=",
+            "condition_value": from_dt.astimezone(pytz.timezone('UTC')).strftime("%Y-%m-%dT%H:%M:%S.000Z")
+        })
+    if to_dt:
+        filters.append({
+            "filter_type": "numeric",
+            "field": "update_datetime",
+            "condition": "<",
+            "condition_value": (to_dt + timedelta(days=1)).astimezone(pytz.timezone('UTC')).strftime("%Y-%m-%dT%H:%M:%S.000Z")
+        })
+  
+    params = {
+        "include_agent_details": "false",
+        "include_debug_info": "false",
+        "filters": json.dumps(filters),
+        "sort": json.dumps([{"update_datetime": "desc"}]),
+        "page_size": 500000
+    }
+    
+    response = requests.get(path, params=params, headers=headers)
+    return response.json()
